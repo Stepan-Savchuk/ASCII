ce18e84c6c4781bdc99ef10401ee2cc74a9d9939
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2022-02-13 20:51:04 +0200

    Initial commit for existing project

---
+++ build/CMakeFiles/ASCII.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "ASCII"
+  "ASCII.pdb"
+  "CMakeFiles/ASCII.dir/ascii.cpp.o"
+  "CMakeFiles/ASCII.dir/ascii.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ASCII.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ASCII.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ASCII.
+# This may be replaced when dependencies are built.
